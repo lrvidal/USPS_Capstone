046d08e8971b77f73edc4f14d4e3fb5cb0195a00
Lucas Ribeiro Vidal <lucas.ribeirovid@gmail.com>
2024-03-27 17:59:47 -0500

    more work

--- DataProvider.py
+++ DataProvider.py
@@ -1,1 +1,64 @@
-# TODO: this is a class that will be used to provide data to the model
+class DataProvider:
+    _airPressureTrend = [0]
+    _airHumidityTrend = [0]
+    _airTemperatureTrend = [0]
+    _voltage = 0
+    _current = 0
+    _power = 0
+    _phaseVoltageTrend = [[0], [0], [0]]
+    _phaseCurrentTrend = [[0], [0], [0]]
+
+    def getCurrentAirPressure(self):
+        #TODO: implement SPI interfacing to acquire data
+        currentPressure = self._airPressureTrend[-1] + 2 #FIXME: this is not real data
+
+        self._airPressureTrend.append(currentPressure)
+        return self._airPressureTrend[-1]
+    
+    def getAirPressureTrend(self):
+        return self._airPressureTrend
+    
+    def getCurrentAirHumidity(self):
+        #TODO: implement SPI interfacing to acquire data
+        currentHumidity = self._airHumidityTrend[-1] + 5 #FIXME: this is not real data
+
+        self._airHumidityTrend.append(currentHumidity)
+        return self._airHumidityTrend[-1]
+    
+    def getAirHumidityTrend(self):
+        return self._airHumidityTrend
+    
+    def getCurrentAirTemperature(self):
+        #TODO: implement SPI interfacing to acquire data
+        currentTemperature = self._airTemperatureTrend[-1] + 1.5 #FIXME: this is not real data
+
+        self._airTemperatureTrend.append(currentTemperature)
+        return self._airTemperatureTrend[-1]
+    
+    def getAirTemperatureTrend(self):
+        return self._airTemperatureTrend
+    
+    def getElectricalAttributes(self):
+        #TODO: implement SPI interfacing to acquire data
+        self._voltage += 1
+        self._current += 0.2
+        self._power += 10
+
+        currentPhaseVoltage1 = self._phaseVoltageTrend[0][-1] + 1
+        currentPhaseCurrent1 = self._phaseCurrentTrend[0][-1] + 1
+        currentPhaseVoltage2 = self._phaseVoltageTrend[1][-1] + 2
+        currentPhaseCurrent2 = self._phaseCurrentTrend[1][-1] + 2
+        currentPhaseVoltage3 = self._phaseVoltageTrend[2][-1] + 3
+        currentPhaseCurrent3 = self._phaseCurrentTrend[2][-1] + 3
+
+        self._phaseVoltageTrend[0].append(currentPhaseVoltage1)
+        self._phaseCurrentTrend[0].append(currentPhaseCurrent1)
+        self._phaseVoltageTrend[1].append(currentPhaseVoltage2)
+        self._phaseCurrentTrend[1].append(currentPhaseCurrent2)
+        self._phaseVoltageTrend[2].append(currentPhaseVoltage3)
+        self._phaseCurrentTrend[2].append(currentPhaseCurrent3)
+
+        return self._voltage, self._current, self._power
+    
+    def getPhaseTrends(self):
+        return self._phaseVoltageTrend, self._phaseCurrentTrend
--- gui.py
+++ gui.py
@@ -3,113 +3,203 @@ from tkinter import ttk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 import matplotlib.pyplot as plt
+from DataProvider import DataProvider
 
-def displayData():
-    # TODO: Replace this with your own data retrieval logic
-    lossOfPressure = [0.5, 0.3, 0.2, 0.4, 0.1]
-    timeToReachPressure = [10, 15, 20, 25, 30]
-    airQuality = ["Good", "Good", "Poor", "Good", "Poor"]
-    oilSystemConditions = ["Normal", "Normal", "Abnormal", "Normal", "Abnormal"]
+dataProvider = DataProvider()
 
-    voltage = [[220, 225, 230, 235, 240], [200, 220, 225, 230, 235], [190, 215, 220, 225, 230]]
-    current = [[1, 2, 3, 4, 5], [5, 4, 3, 2, 1], [1, 5, 1, 5, 1]]
-    power = [[2200, 2205, 2300, 2305, 2400], [2000, 2200, 2025, 2300, 2305], [1900, 2150, 2200, 2025, 2300]]
-    phaseVoltage = [[220, 225, 230, 235, 240], [200, 220, 225, 230, 235], [190, 215, 220, 225, 230]]
-    phaseCurrent = [[1, 2, 3, 4, 5], [5, 4, 3, 2, 1], [1, 5, 1, 5, 1]]
+class GUI:
+    def __init__(self, window, notebook):
+        self.window = window
+        self.notebook = notebook
+        self.dataProvider = DataProvider()
+        self.electrical_tab = ttk.Frame(notebook)
+        self.air_tab = ttk.Frame(notebook)
+        self.oil_tab = ttk.Frame(notebook)
+        self.download_tab = ttk.Frame(notebook)
 
-    # Create a new window
-    window = tk.Tk()
+        notebook.add(self.electrical_tab, text="Electrical")
+        notebook.add(self.oil_tab, text="Oil")
+        notebook.add(self.air_tab, text="Air")
+        notebook.add(self.download_tab, text="Download")
 
-    # Create a notebook widget to hold the tabs
-    notebook = ttk.Notebook(window)
+        self.electrical_fig, self.electrical_axs = plt.subplots(3, 2, figsize=(10, 10))
+        self.electrical_canvas = FigureCanvasTkAgg(self.electrical_fig, master=self.electrical_tab)
 
-    # Create tabs for electrical, oil, and air
-    electrical_tab = ttk.Frame(notebook)
-    oil_tab = ttk.Frame(notebook)
-    air_tab = ttk.Frame(notebook)
+        self.oil_fig, self.oil_axs = plt.subplots(1, 2, figsize=(10, 5))
+        self.oil_canvas = FigureCanvasTkAgg(self.oil_fig, master=self.oil_tab)
 
-    # Add the tabs to the notebook
-    notebook.add(electrical_tab, text="Electrical")
-    notebook.add(oil_tab, text="Oil")
-    notebook.add(air_tab, text="Air")
+        self.air_fig, self.air_axs = plt.subplots(3, 2, figsize=(10, 10))
+        self.air_canvas = FigureCanvasTkAgg(self.air_fig, master=self.air_tab)
 
-    # Create a figure and subplots for each data in the electrical tab
-    electrical_fig, electrical_axs = plt.subplots(3, 2, figsize=(10, 10))
+        self.button = tk.Button(self.download_tab, text="Download Trends as CSV", command=self.fileDownload)
+        self.button.pack()
+        self.button.place(relx=0.4, rely=0.4, relheight=0.1, relwidth=0.3)
 
-    # Plot voltage in the electrical tab
-    for i in range(3):
-        electrical_axs[0, 0].plot(voltage[i], label=f"Phase {i+1}")
-        electrical_axs[0, 1].plot(current[i], label=f"Phase {i+1}")
-        electrical_axs[1, 0].plot(power[i], label=f"Phase {i+1}")
-        electrical_axs[1, 1].plot(phaseVoltage[i], label=f"Phase {i+1}")
-        electrical_axs[2, 0].plot(phaseCurrent[i], label=f"Phase {i+1}")
 
-    electrical_axs[0, 0].set_title("Voltage")
-    electrical_axs[0, 0].legend()
+    def fileDownload(self):
+        return
 
-    electrical_axs[0, 1].set_title("Current")
-    electrical_axs[0, 1].legend()
+    def displayData(self):
+        # TODO: Replace this with your own data retrieval logic
 
-    electrical_axs[1, 0].set_title("Power")
-    electrical_axs[1, 0].legend()
+        lossOfPressure = [0.5, 0.3, 0.2, 0.4, 0.1]
+        oilSystemConditions = ["Normal", "Normal", "Abnormal", "Normal", "Abnormal"]
 
-    electrical_axs[1, 1].set_title("Phase Voltage")
-    electrical_axs[1, 1].legend()
+        # Add the tabs to the notebook
+        self.notebook.add(self.electrical_tab, text="Electrical")
+        self.notebook.add(self.oil_tab, text="Oil")
+        self.notebook.add(self.air_tab, text="Air")
 
-    electrical_axs[2, 0].set_title("Phase Current")
-    electrical_axs[2, 0].legend()
+        # Plot voltage in the electrical tab
+        self.updateElectricalAttributes()
+        self.updatePhaseTrends()
+        # Adjust spacing between subplots in the electrical tab
+        self.electrical_fig.tight_layout()
 
-    # Adjust spacing between subplots in the electrical tab
-    electrical_fig.tight_layout()
+        self.electrical_canvas.draw()
+        self.electrical_canvas.get_tk_widget().pack()
 
-    # Create a label to display the electrical figure in the electrical tab
-    electrical_canvas = FigureCanvasTkAgg(electrical_fig, master=electrical_tab)
-    electrical_canvas.draw()
-    electrical_canvas.get_tk_widget().pack()
+        # Plot loss of pressure over time in the oil tab
+        self.oil_axs[0].plot(lossOfPressure)
+        self.oil_axs[0].set_title("Loss of Pressure")
 
-    # Create a figure and subplots for each data in the oil tab
-    oil_fig, oil_axs = plt.subplots(1, 2, figsize=(10, 5))
+        # Plot oil system conditions in the oil tab
+        self.oil_axs[1].plot(oilSystemConditions)
+        self.oil_axs[1].set_title("Oil System Conditions")
 
-    # Plot loss of pressure over time in the oil tab
-    oil_axs[0].plot(lossOfPressure)
-    oil_axs[0].set_title("Loss of Pressure")
+        # Adjust spacing between subplots in the oil tab
+        self.oil_fig.tight_layout()
 
-    # Plot oil system conditions in the oil tab
-    oil_axs[1].plot(oilSystemConditions)
-    oil_axs[1].set_title("Oil System Conditions")
+        # Create a label to display the oil figure in the oil tab
+        self.oil_canvas.draw()
+        self.oil_canvas.get_tk_widget().pack()
 
-    # Adjust spacing between subplots in the oil tab
-    oil_fig.tight_layout()
+        # Plot current air pressure in the air tab
+        self.updateCurrentAirPressure()
 
-    # Create a label to display the oil figure in the oil tab
-    oil_canvas = FigureCanvasTkAgg(oil_fig, master=oil_tab)
-    oil_canvas.draw()
-    oil_canvas.get_tk_widget().pack()
+        # Plot air pressure trend in the air tab
+        self.updateAirPressureTrend()
 
-    # Create a figure and subplots for each data in the air tab
-    air_fig, air_axs = plt.subplots(2, 1, figsize=(5, 10))
+        # Plot current air humidity in the air tab
+        self.updateCurrentAirHumidity()
 
-    # Plot time to reach operating pressure in the air tab
-    air_axs[0].plot(timeToReachPressure)
-    air_axs[0].set_title("Time to Reach Operating Pressure")
+        # Plot air humidity trend in the air tab
+        self.updateAirHumidityTrend()
 
-    # Plot air quality conditions in the air tab
-    air_axs[1].plot(airQuality)
-    air_axs[1].set_title("Air Quality")
+        # Plot current air temperature in the air tab
+        self.updateCurrentAirTemperature()
 
-    # Adjust spacing between subplots in the air tab
-    air_fig.tight_layout()
+        # Plot air temperature trend in the air tab
+        self.updateAirTemperatureTrend()
 
-    # Create a label to display the air figure in the air tab
-    air_canvas = FigureCanvasTkAgg(air_fig, master=air_tab)
-    air_canvas.draw()
-    air_canvas.get_tk_widget().pack()
+        # Adjust spacing between subplots in the air tab
+        self.air_fig.tight_layout()
 
-    # Pack the notebook widget
-    notebook.pack()
 
-    # Run the GUI event loop
-    window.mainloop()
+        self.air_canvas.draw()
+        self.air_canvas.get_tk_widget().pack()
 
-# Call the function to display the data
-displayData()
+        # Pack the notebook widget
+        self.notebook.pack()
+        # Run the GUI event loop
+
+        self.window.after(3000, self.updateData) #FIXME: time interval for updating data is hardcoded
+        self.window.mainloop()
+
+    def updateData(self):
+        self.updateCurrentAirPressure()
+        self.updateAirPressureTrend()
+        self.updateCurrentAirHumidity()
+        self.updateAirHumidityTrend()
+        self.updateCurrentAirTemperature()
+        self.updateAirTemperatureTrend()
+        self.updateElectricalAttributes()
+        self.updatePhaseTrends()
+
+        self.window.after(3000, self.updateData) #FIXME: time interval for updating data is hardcoded
+
+    def updateAirHumidityTrend(self):
+        self.air_axs[1, 1].cla()
+        self.air_axs[1, 1].plot(dataProvider.getAirHumidityTrend())
+        self.air_axs[1, 1].set_title("Air Humidity Trend")
+        self.air_canvas.draw()
+
+    def updateCurrentAirHumidity(self):
+        self.air_axs[1, 0].cla()
+        self.air_axs[1, 0].set_title("Current Air Humidity")
+        self.air_axs[1, 0].set_xticks([])
+        self.air_axs[1, 0].set_yticks([])
+        self.air_axs[1, 0].text(0.5, 0.5, str(dataProvider.getCurrentAirHumidity()) + " %RH", fontsize=35, ha='center')
+        self.air_canvas.draw()
+
+    def updateAirPressureTrend(self):
+        self.air_axs[0, 1].cla()
+        self.air_axs[0, 1].plot(dataProvider.getAirPressureTrend())
+        self.air_axs[0, 1].set_title("Air Pressure Trend")
+        self.air_canvas.draw()
+
+    def updateCurrentAirPressure(self):
+        self.air_axs[0, 0].cla()
+        self.air_axs[0, 0].set_title("Current Air Pressure")
+        self.air_axs[0, 0].set_xticks([])
+        self.air_axs[0, 0].set_yticks([])
+        self.air_axs[0, 0].text(0.5, 0.5, str(dataProvider.getCurrentAirPressure()) + " psi", fontsize=35, ha='center')
+        self.air_canvas.draw()
+    
+    def updateAirTemperatureTrend(self):
+        self.air_axs[2, 1].cla()
+        self.air_axs[2, 1].plot(dataProvider.getAirTemperatureTrend())
+        self.air_axs[2, 1].set_title("Air Temperature Trend")
+        self.air_canvas.draw()
+
+    def updateCurrentAirTemperature(self):
+        self.air_axs[2, 0].cla()
+        self.air_axs[2, 0].set_title("Current Air Temperature")
+        self.air_axs[2, 0].set_xticks([])
+        self.air_axs[2, 0].set_yticks([])
+        self.air_axs[2, 0].text(0.5, 0.5, str(dataProvider.getCurrentAirTemperature()) + " °F", fontsize=35, ha='center')
+        self.air_canvas.draw()
+
+    def updateElectricalAttributes(self):
+        voltage, current, power = dataProvider.getElectricalAttributes()
+
+        self.electrical_axs[0, 0].cla()
+        self.electrical_axs[0, 0].set_title("Current Voltage")
+        self.electrical_axs[0, 0].set_xticks([])
+        self.electrical_axs[0, 0].set_yticks([])
+        self.electrical_axs[0, 0].text(0.5, 0.5, str(voltage) + " V", fontsize=35, ha='center')
+
+        self.electrical_axs[0, 1].cla()
+        self.electrical_axs[0, 1].set_title("Current Current")
+        self.electrical_axs[0, 1].set_xticks([])
+        self.electrical_axs[0, 1].set_yticks([])
+        self.electrical_axs[0, 1].text(0.5, 0.5, str(current) + " A", fontsize=35, ha='center')
+
+        self.electrical_axs[1, 0].cla()
+        self.electrical_axs[1, 0].set_title("Current Power")
+        self.electrical_axs[1, 0].set_xticks([])
+        self.electrical_axs[1, 0].set_yticks([])
+        self.electrical_axs[1, 0].text(0.5, 0.5, str(power) + " kW", fontsize=35, ha='center')
+
+        self.electrical_canvas.draw()
+
+    def updatePhaseTrends(self):
+        self.electrical_axs[1, 1].cla()
+        self.electrical_axs[2, 0].cla()
+        phaseVoltage, phaseCurrent = dataProvider.getPhaseTrends()
+
+        for i in range(3):
+            self.electrical_axs[1, 1].plot(phaseVoltage[i], label=f"Phase {i+1}")
+            self.electrical_axs[2, 0].plot(phaseCurrent[i], label=f"Phase {i+1}")
+
+        self.electrical_axs[1, 1].set_title("Phase Voltage")
+        self.electrical_axs[1, 1].legend()
+
+        self.electrical_axs[2, 0].set_title("Phase Current")
+        self.electrical_axs[2, 0].legend()
+
+window = tk.Tk()
+notebook = ttk.Notebook(window)
+gui = GUI(window=window, notebook=notebook)
+
+gui.displayData()
