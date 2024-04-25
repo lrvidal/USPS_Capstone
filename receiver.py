3f5dcb0bb5ccbe548ee48fa2bff6fa523b6e2576
Lucas Ribeiro Vidal <lucas.ribeirovid@gmail.com>
2024-04-25 11:13:11 -0500

    server listening

---
+++ receiver.py
@@ -0,0 +1,39 @@
+import socket
+
+# The IP address of the server (this should be the IP address of the receiving machine)
+HOST = '0.0.0.0'  # This will listen on all available network interfaces
+PORT = 5000
+
+# Create a socket
+s = socket.socket()
+
+# Bind the socket to the IP address and port
+s.bind((HOST, PORT))
+
+# Listen for incoming connections
+s.listen()
+
+print('Server is listening...')
+
+# Accept an incoming connection
+conn, addr = s.accept()
+
+print('Connected by', addr)
+
+# Open a file in binary mode to write the incoming data
+with open('received_data.csv', 'wb') as f:
+    while True:
+        # Receive data from the client
+        data = conn.recv(1024)
+
+        # If no more data is received, break the loop
+        if not data:
+            break
+
+        # Write the received data to the file
+        f.write(data)
+
+# Close the connection
+conn.close()
+
+print('File received.')
