36ede82e3bf9983e7459b4f6311703d315b792c1
Lucas Ribeiro Vidal <lucas.ribeirovid@gmail.com>
2024-04-25 22:32:16 -0500

    mre

--- SPI.py
+++ SPI.py
@@ -2,6 +2,7 @@ import spidev
 
 class SPIDevice:
     def __init__(self, bus, device):
+        self.device = device
         self.spi = spidev.SpiDev()
         self.spi.open(bus, device)
         self.spi.max_speed_hz = 1000000  # Set SPI clock speed (1 MHz)
