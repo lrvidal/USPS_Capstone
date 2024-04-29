23977787ba53c1122a89f08c8aecab057b7bdf98
Lucas Ribeiro Vidal <lucas.ribeirovid@gmail.com>
2024-04-29 12:27:02 -0500

    error handle

--- gui.py
+++ gui.py
@@ -294,7 +294,7 @@ CONCERNING_PARAMS = {
     "Air Temperature": AIR_TEMP_CONCERNING
 }
 def isMeasurementConcerning(type, value):
-    return True #if type(value) is not str else float(value) > CONCERNING_PARAMS[type]
+    return 1 #if type(value) is not str else float(value) > CONCERNING_PARAMS[type]
 
 window = tk.Tk()
 window.title("DUS Monitoring System")
