73cbeec2ff0413e29aae1479bdc33c903c8fac63
Lucas Ribeiro Vidal <lucas.ribeirovid@gmail.com>
2024-04-29 13:10:30 -0500

    local echo false attempt

--- ModBus.py
+++ ModBus.py
@@ -1,19 +1,20 @@
 import minimalmodbus
+import time
 
 class ModBus:
-    def __init__(self, port, address) :
-    # Initialize Modbus Reader
-        self.instr = minimalmodbus.Instrument(port,address)
+    def __init__(self, port, address):
+        # Initialize Modbus Reader
+        self.instr = minimalmodbus.Instrument(port, address)
         self.instr.serial.baudrate = 9600
         self.instr.handle_local_echo = False
 
     def read_modbus_float(self, register):
         for i in range(10):
             try:
+                time.sleep(0.5)  # Add a delay of 0.5 seconds
                 value = self.instr.read_float(register)
-                return round(value,4)
+                return round(value, 4)
             except Exception as e:
                 value = None
                 print(e)
         return value
-
